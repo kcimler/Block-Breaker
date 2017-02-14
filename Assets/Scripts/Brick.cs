c1cd7ff716dd3ca9bda9b58719259a706d504e0c
DESKTOP-IOJT21N\kciml <kcimler@luc.edu>
2017-02-13 18:13:03 -0600

    First Push to Github

---
+++ Assets/Scripts/Brick.cs
@@ -0,0 +1,70 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Brick : MonoBehaviour {
+
+	public AudioClip crack;
+	public Sprite[] hitSprites;
+	public static int brickCount = 0;
+	public GameObject smoke;
+	
+	private int timesHit;
+	private LevelManager levelManager;
+	private bool isBreakable;
+
+	// Use this for initialization
+	void Start () {
+		isBreakable = (this.tag == "Breakable");
+		// keep track of breakable bricks
+		if (isBreakable) {
+			brickCount++;
+		} 
+		timesHit = 0;
+		levelManager = GameObject.FindObjectOfType<LevelManager>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+	
+	void OnCollisionEnter2D (Collision2D collision){
+		AudioSource.PlayClipAtPoint (crack, transform.position, .8f);
+		if (isBreakable){
+			HandleHits();
+		}
+	}
+	
+	void HandleHits(){
+		timesHit++;
+		int maxHits = hitSprites.Length + 1;
+		if(timesHit >= maxHits){
+			brickCount--;
+			Debug.Log (brickCount);
+			levelManager.BrickDestroyed();
+			puffSmoke();
+			Destroy(gameObject);
+		} else {
+			LoadSprites();
+		}
+	}
+	
+	void puffSmoke(){
+		GameObject Puff = Instantiate (smoke,transform.position, Quaternion.identity) as GameObject;
+		Puff.particleSystem.startColor = gameObject.GetComponent<SpriteRenderer>().color;
+	}
+	
+	void LoadSprites() {
+		int spriteIndex = timesHit - 1;
+		if (hitSprites[spriteIndex]){
+			this.GetComponent<SpriteRenderer>().sprite = hitSprites[spriteIndex];
+		} else {
+			Debug.LogError("MISSING SPRITE");
+		}
+	}
+	
+	// TODO Remove this method once we actually win!
+	void SimulateWin () {
+		levelManager.LoadNextLevel();
+	}
+}
---
+++ Assets/Scripts/LevelManager.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LevelManager : MonoBehaviour {
+	
+	public void LoadLevel(string name){
+		Debug.Log ("Level load requested for: " + name);
+		Brick.brickCount = 0;
+		Application.LoadLevel(name);
+	}
+	
+	public void quitLevel() {
+		Debug.Log ("Quit function requested");
+		Application.Quit();
+	}
+	
+	public void LoadNextLevel() {
+		Brick.brickCount = 0;
+		Application.LoadLevel(Application.loadedLevel+1);
+	}
+	
+	public void BrickDestroyed () {
+		if (Brick.brickCount <= 0) {
+			print ("Called for next level");
+			LoadNextLevel();
+		}
+	}
+}
